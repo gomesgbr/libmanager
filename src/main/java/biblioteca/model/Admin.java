394e35e22be69de132e6e5a168a6b74f6733a1a8
Gabriel Almeida <gabrielg299@gmail.com>
2023-09-21 21:54:45 -0300

    Admin model adicionado

---
+++ src/main/java/biblioteca/model/Admin.java
@@ -0,0 +1,55 @@
+package biblioteca.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_admin")
+public class Admin implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private int id;
+    @Column
+    private String nome;
+    @Column
+    private String senha;
+
+    public Admin(){
+
+    }
+
+    public Admin(int id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+  
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}

44cdc574c16a5963f381388e5c16687eee11a749
Gabriel Almeida <gabrielg299@gmail.com>
2023-09-25 22:50:16 -0300

    Criptografar concertado

--- src/main/java/biblioteca/security/CriptografarPwd.java
+++ src/main/java/biblioteca/security/CriptografarPwd.java
@@ -4,7 +4,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class CriptografarPwd {
 
-    public String crypt(String passwd){
+    public static String crypt(String passwd){
         String salt = BCrypt.gensalt();
 
         String hashPasswd = BCrypt.hashpw(passwd, salt);
@@ -12,8 +12,8 @@ public class CriptografarPwd {
         return hashPasswd;
     }
 
-    public boolean validHashPasswd(String hash, String passwd){
-        return BCrypt.checkpw(hash, passwd);
+    public static boolean validHashPasswd(String hash, String passwd){
+        return BCrypt.checkpw(passwd, hash);
     }
     
 }
